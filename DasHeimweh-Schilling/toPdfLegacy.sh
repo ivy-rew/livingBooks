305761393207ac86212271f38f078433084eda55
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2019-06-18 18:42:55 +0200

    toolchain for PDF with original imgs+ocr text

---
+++ DasHeimweh-Schilling/toPdfLegacy.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+rm -rf out
+mkdir out
+mkdir out/joined
+dir="$(pwd)"
+
+#deps
+sudo apt install texlive-xetex texlive-fonts-recommended
+
+function pagePdf()
+{
+    #pdf with original image + revised OCR text
+    md=$1
+    bmd=`basename $md`
+    page=${bmd#*schilling}
+    #=page-XXX-XYZ
+    page=${page:0:12}
+    no=${page:5:3}
+    img=out/joined/page-$no.img.pdf
+    echo "img pdf: $img"
+    pdfseparate -f $no -l $no ../johannheinrichj04grolgoog.pdf $img
+    echo "ocr pdf: $md"
+    pandoc $md --latex-engine=xelatex -V geometry:a6paper -o out/$bmd.pdf
+    echo "merging: $img out/$bmd.pdf"
+    pdfunite $img out/$bmd.pdf  out/joined/translated-page-$no.pdf
+}
+
+function uniteChapter()
+{
+    container="$1"
+    for md in `ls -v $container/schilling*.md`
+    do
+        pagePdf $md
+    done
+    pages=`ls -v out/joined/translated-page*.pdf`
+    echo "book: $pages"
+    pdfunite $pages out/joined/translated-$container.pdf
+}
+
+#pagePdf 0intro/schillingpage-016-018.pbm.txt.md
+
+#unite em all...
+uniteChapter 0intro
+
+#pagePdf 0intro/schillingpage-016-018.pbm.txt.md
+
+
+
+
