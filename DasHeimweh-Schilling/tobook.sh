e77ee0164b1f63e3c152147fd8b215620d8df118
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2019-06-20 13:29:40 +0200

    re-use booksAlive scripts to create book
    - download pdf from archive.org instead of extract images under SCM
    - less local disk by using PNG images (best quality by smalles file size)

--- DasHeimweh-Schilling/tobook.sh
+++ DasHeimweh-Schilling/tobook.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
-rm -rf out
-mkdir out
 dir="$(pwd)"
 
-function collectMarkdown()
-{
-    $txtBook = "$1"
-    for text in `ls -v schilling*.md`
-    do
-        echo "processing "+$text
-        echo "<div id=$text>" >> "$txtBook"
-        cat $text >> "$txtBook"
-        echo "</div>" >> "$txtBook"
-    done
-    echo "created $txtBook"
-}
+# https://openlibrary.org/books/OL20522092M/
+if ! [ -f johannheinrichj04grolgoog.pdf ]; then
+    wget https://archive.org/download/johannheinrichj04grolgoog/johannheinrichj04grolgoog.pdf
+fi
 
-cd book1
-txtBook="$dir/out/book1.md"
-collectMarkdown $txtBoox
+source ~/booksAlive/bookUtils.sh . johannheinrichj04grolgoog.pdf
+extractPdfImages
 
-cd "$dir"
-txtBook="$dir/out/full.md"
-collectMarkdown $txtBoox
 
-echo "creating epub"
-pandoc -o out/schilling.epub 0-intro.md out/book1.md out/full.md
