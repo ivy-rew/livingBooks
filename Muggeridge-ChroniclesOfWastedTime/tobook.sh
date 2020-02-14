7343b3f4f8ba8585cfe2dc3429bb56e8c83db1ab
LordEagle <r.wermelinger@gmail.com>
2020-02-14 17:27:24 +0100

    styled titles

--- Muggeridge-ChroniclesOfWastedTime/tobook.sh
+++ Muggeridge-ChroniclesOfWastedTime/tobook.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 dir="$(pwd)"
 
+source ~/dev/booksAlive/bookUtils.sh . mug.pdf
 
 function provideSources()
 {
     # https://openlibrary.org/books/OL20522092M/
-    if ! [ -f mug.pdf ]; then
+    if ! [ -f $pdf ]; then
         wget https://ia600206.us.archive.org/22/items/MuggeridgeMalcolmChroniclesOfWastedTime/Muggeridge%2C%20Malcolm%20-%20Chronicles%20of%20Wasted%20Time.pdf
-        ln -s "Muggeridge, Malcolm - Chronicles of Wasted Time.pdf" mug.pdf
+        ln -s "Muggeridge, Malcolm - Chronicles of Wasted Time.pdf" $pdf
+        provideSources
     fi
 }
 
-provideSources
 
-source ~/dev/booksAlive/bookUtils.sh . mug.pdf
 #extractPdfImages
 #ocrFractured "eng"
 
@@ -23,23 +23,44 @@ function collectMarkdown2()
     book=$bookName.md
     mkdir -p "$bookdir"
     bookfile="$bookdir/$book"
+    rm $bookfile
     for text in `ls -v $textdir/*/*.md`
     do
         #skip empty files...
         echo "processing "+$text
         #echo "<div id=\"$text\">" >> "$bookfile"
-        cat $text | sed -E 's|^([0-9]+[A-Za-z ]+\|[A-Za-z ]+[0-9]+)|<span class=\"site\">\1</span>|' >> "$bookfile"
+
+        # mark titles with a 'pos' style
+        cat $text \
+          | sed -E '0,/^([0-9]{1,3} [A-Z][A-Za-z ]+|[A-Z][A-Za-z ]+[0-9]{1,3}$)/s||<span class=\"pos\">\1</span>|' \
+          >> "$bookfile"
+
         #echo "</div>" >> "$bookfile"
     done
     echo "created $book"
 }
 
-#collectMarkdown2
+collectMarkdown2
 #source ~/booksAlive/pdfProdUtils.sh $dir
+
+
+#pandoc --epub-cover-image="$WORK/pages/page-001-000.png" --epub-chapter-level=2 -f markdown -o "$bookdir/mug.epub" "$WORK/meta.md" "$bookdir/mug-html.md"
+
 
 function epub()
 {
-    pandoc --epub-cover-image="$WORK/pages/page-001-000.png" --epub-chapter-level=2 -f markdown -o "$bookdir/mug.epub" "$WORK/meta.md" "$bookdir/mug-html.md"
+    pandoc --epub-cover-image="$WORK/pages/page-001-000.png" \
+     --epub-chapter-level=2 \
+     --epub-stylesheet="$WORK/style.css" \
+     -f markdown -o "$bookdir/mug.epub" \
+     "$WORK/meta.md" \
+     "$bookdir/mug.md" \
+     $(ls -v text/*/*.md)
+}
+
+function kindle()
+{
+    ebook-convert "$bookdir/mug.epub" "$bookdir/mug.azw3"
 }
 
 epub
