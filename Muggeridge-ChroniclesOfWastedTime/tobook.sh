de691fdae35598bbba662dd5c617d67025b28dc1
LordEagle <r.wermelinger@gmail.com>
2020-02-22 13:27:12 +0100

    move common md processing to bookUtils.sh

--- Muggeridge-ChroniclesOfWastedTime/tobook.sh
+++ Muggeridge-ChroniclesOfWastedTime/tobook.sh
@@ -22,56 +22,41 @@ function provideSources()
     fi
 }
 
-function transformMarkdown()
+function improveMd()
 {
-    tdir="$DIR/trans"
-    rm -r -v "$tdir"
-    mkdir -p "$tdir"
-    rm "$bookfile"
-
-    cd "$textdir"
-    for text in `ls -v */*.md`
-    do
-        echo "processing "$text
-
-        parent=$(dirname "$text")
-        mkdir -p "$tdir/$parent"
-        target="$tdir/$text"
-
-        file=$(basename $text)
-        cleaned=$(find "$DIR/clean" -name "$file")
-        if ! [ -z "$cleaned" ]; then
-            text="$cleaned" #prefer local modified to generated
-        fi
-
-        # sed1-3: fix frequent false positives from OCR
-        # sed4: remove hyphens at the end of the a line
-        # sed5: mark titles with a 'pos' style
-        # sed6: remove section brakes by wrong detected white spaces
-        cat $text \
-          | sed 's/Who Whom? = /Who Whom? /' \
-          | sed 's/ ©The Infernal Grove/ The Infernal Grove\n/' \
-          | sed 's/The Victor’s Camp = /The Victor’s Camp /' \
-          | sed ':a;N;$!ba;s/-\n//g' \
-          | sed -E '0,/^([0-9]{1,3} [A-Z][A-Za-z’? ]+|[A-Z][A-Za-z’? ]+[0-9]{1,3}$)/s||<span class=\"pos\">\1</span>|' \
-          | sed -E ':a;N;$!ba;s/([a-z,])\n\n([a-z])/\1 \2/g' \
-          >> "$target"
-    done
-
-    cd "$DIR"
+    textIn="$1"
+    # sed1-3: fix frequent false positives from OCR
+    # sed4: remove hyphens at the end of the a line
+    # sed5: mark titles with a 'pos' style
+    # sed6: remove section brakes by wrong detected white spaces
+    cat "$textIn" \
+        | sed 's/Who Whom? = /Who Whom? /' \
+        | sed 's/ ©The Infernal Grove/ The Infernal Grove\n/' \
+        | sed 's/The Victor’s Camp = /The Victor’s Camp /' \
+        | sed ':a;N;$!ba;s/-\n//g' \
+        | sed -E '0,/^([0-9]{1,3} [A-Z][A-Za-z’? ]+|[A-Z][A-Za-z’? ]+[0-9]{1,3}$)/s||<span class=\"pos\">\1</span>|' \
+        | sed -E ':a;N;$!ba;s/([a-z,])\n\n([a-z])/\1 \2/g'
 }
 
 function epub()
 {
+    MD_FIND='trans/*.md'
+    if [ ! -z "$1" ]; then
+        MD_FIND=$1
+    fi
+
     pandoc --epub-cover-image="$WORK/pages/page-001-000.png" \
      --epub-chapter-level=2 \
      --epub-stylesheet="$WORK/style.css" \
      -f markdown -o "$bookdir/$bookName.epub" \
      "$WORK/meta.md" \
-     $(ls -v trans/*/*.md)
+     $(ls -v ${MD_FIND})
 }
 
-provideSources
-transformMarkdown
-epub
-bookToKindle
+if ! [ "$1" == "test" ]; then
+    provideSources
+    transformMarkdown "*/*.md"
+    epub "trans/*/*.md"
+    bookToKindle
+fi
+
